c4f7619f7a1077b81de125b8dbc4949cdffbd3d9
abbywong <905418235@qq.com>
2016-12-03 22:53:35 +0100

    empty fragment shows up?

--- app/src/main/java/com/example/android/fragments/MainActivity.java
+++ app/src/main/java/com/example/android/fragments/MainActivity.java
@@ -8,6 +8,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_titles);
+//        setContentView(R.layout.fragment_titles);
+
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//
+//            setContentView(R.layout.activity_main);
+//        }
     }
 }
