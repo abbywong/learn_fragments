6b30450271f708610edc6d33a983a8d6b54c85d8
abbywong <905418235@qq.com>
2016-12-04 17:53:38 +0100

    fragment inside activity

--- app/src/main/java/com/example/android/fragments/MainActivity.java
+++ app/src/main/java/com/example/android/fragments/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.android.fragments;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_titles);
+        setContentView(R.layout.activity_main);
 //        setContentView(R.layout.fragment_titles);
 
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        TitlesFragment fragContent = new TitlesFragment();
+        fragmentTransaction.add(R.id.FragmentContainer, fragContent);
+        fragmentTransaction.commit();
+
 //        @Override
-//        protected void onCreate(Bundle savedInstanceState) {
 //            super.onCreate(savedInstanceState);
 //
 //            setContentView(R.layout.activity_main);
