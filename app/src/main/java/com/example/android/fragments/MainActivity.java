6f4002bc68da53b727c83c5d5339a738c1a66454
abbywong <905418235@qq.com>
2016-12-04 18:13:57 +0100

    fragment import fix

--- app/src/main/java/com/example/android/fragments/MainActivity.java
+++ app/src/main/java/com/example/android/fragments/MainActivity.java
@@ -3,21 +3,25 @@ package com.example.android.fragments;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+//import android.support.v4.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);}
 //        setContentView(R.layout.fragment_titles);
-
+    public void onFragmentActivated(View v) {
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        TitlesFragment fragContent = new TitlesFragment();
-        fragmentTransaction.add(R.id.FragmentContainer, fragContent);
+        TitlesFragment fragment = new TitlesFragment();
+//        TitlesFragment fragment2 = new TitlesFragment();
+//        TitlesFragment fragment3 = new TitlesFragment();
+        fragmentTransaction.add(R.id.FragmentContainer, fragment);
         fragmentTransaction.commit();
 
 //        @Override
@@ -25,5 +29,5 @@ public class MainActivity extends AppCompatActivity {
 //
 //            setContentView(R.layout.activity_main);
 //        }
-    }
-}
+    }}
+
